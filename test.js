1288a098e9140d97c8f51ff467911794a750e11f
Josue Padilla <cjosuepadilla123@gmail.com>
2025-12-17 22:35:10 -0400

    Remove unnecessary files, keep only index.js and package.json

--- test.js
+++
@@ -1,2 +0,0 @@
-const url = 'https://api.telegram.org/bot8386480773:AAGjmVBwInEFuCTV4-9Msbw-u0zVD6i3F0U/getMe';
-fetch(url).then(r=>r.text()).then(t=>console.log(t)).catch(e=>console.error('fail', e));
